1b426b2d37abf59545fe4e8f2be72ecb0a8da5ee
alifanov <lifanov.a.v@gmail.com>
2017-04-22 17:20:40 +0300

    π

--- api/views.py
+++ api/views.py
@@ -57,11 +57,11 @@ class WebhookView(APIView):
 
 @webhook_bot.message_handler(func=lambda message: True, content_types=['text', 'sticker'])
 def echo_message(message):
-    print(message.text)
-    print(message.text == '/offer')
-    print(message)
-    print(message.chat.id)
     bot_user, _ = BotUser.objects.get_or_create(chat_id=message.chat.id)
+    if _:
+        Bot.objects.create(
+            user=bot_user
+        )
     if message.text == '/start':
         markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
         markup.add('/offer')
