66c093495d983a82eb5a130527f3901eacce7069
alifanov <lifanov.a.v@gmail.com>
2017-04-22 16:01:41 +0300

    add handler by webhook

--- api/views.py
+++ api/views.py
@@ -51,8 +51,8 @@ class WebhookView(APIView):
 
 @webhook_bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    webhook_bot.send_sticker(message.chat.id, open(os.path.join(BASE_DIR, 'images', 'start.png')).read())
     webhook_bot.reply_to(message, message.text)
+    webhook_bot.send_sticker(message.chat.id, open(os.path.join(BASE_DIR, 'images', 'start.png')).read())
 
 
 @webhook_bot.message_handler(func=lambda message: True, content_types=['start'])
